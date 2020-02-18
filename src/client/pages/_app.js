33ab406e2f8a6cae39828a662e3303c125e32f5d
Scottie Schneider <scott@laclave.co>
2020-02-17 22:28:52 -0500

    successful instantiation of the db with white label

--- src/client/pages/_app.js
+++ src/client/pages/_app.js
@@ -1,22 +1,22 @@
-import React, { createContext, useContext } from "react";
-import App from "next/app";
-import Page from "../components/Page";
-import db from "../lib/db";
-import absoluteUrl from "next-absolute-url";
+import React, { createContext, useContext } from "react"
+import App from "next/app"
+import Page from "../components/Page"
+import db from "../lib/db"
+import absoluteUrl from "next-absolute-url"
 
-const TenantContext = createContext(null);
-export { TenantContext };
+const TenantContext = createContext(null)
+export { TenantContext }
 
 export default class MyApp extends App {
   static async getInitialProps({ Component, router, ctx }) {
-    const absolute = absoluteUrl(ctx.req);
-    let pageProps = {};
+    const absolute = absoluteUrl(ctx.req)
+    let pageProps = {}
     // fallback tenant object used as a default
     let tenantObject = {
       tagline: "Didn't find a url",
       imageURL:
-        "https://helpx.adobe.com/content/dam/help/en/stock/how-to/visual-reverse-image-search/jcr_content/main-pars/image/visual-reverse-image-search-v2_intro.jpg"
-    };
+        "https://helpx.adobe.com/content/dam/help/en/stock/how-to/visual-reverse-image-search/jcr_content/main-pars/image/visual-reverse-image-search-v2_intro.jpg",
+    }
 
     try {
       await db(absolute.host)
@@ -24,50 +24,54 @@ export default class MyApp extends App {
         .collection("tenant")
         .limit(1)
         .get()
-        .then(docs => {
+        .then((docs) => {
           if (docs.empty) {
             tenantObject = {
               tagline: "no doc found",
               imageURL:
-                "https://helpx.adobe.com/content/dam/help/en/stock/how-to/visual-reverse-image-search/jcr_content/main-pars/image/visual-reverse-image-search-v2_intro.jpg"
-            };
-            return;
+                "https://helpx.adobe.com/content/dam/help/en/stock/how-to/visual-reverse-image-search/jcr_content/main-pars/image/visual-reverse-image-search-v2_intro.jpg",
+            }
+            return
           }
-          docs.forEach(doc => {
-            console.log(doc.id, doc.data());
-            tenantObject = doc.data();
-          });
+          docs.forEach((doc) => {
+            console.log(doc.id, doc.data())
+            tenantObject = doc.data()
+          })
         })
-        .catch(err => {
+        .catch((err) => {
           tenantObject = {
             tagline: "there is an error inside catch",
-            imageURL: "https://helpx.adobe.com/content/dam/help/en/stock/how-to/visual-reverse-image-search/jcr_content/main-pars/image/visual-reverse-image-search-v2_intro.jpg"
+            imageURL:
+              "https://helpx.adobe.com/content/dam/help/en/stock/how-to/visual-reverse-image-search/jcr_content/main-pars/image/visual-reverse-image-search-v2_intro.jpg",
           }
-        });
+        })
     } catch (err) {
       tenantObject = {
         tagline: err,
-        imageURL: "https://helpx.adobe.com/content/dam/help/en/stock/how-to/visual-reverse-image-search/jcr_content/main-pars/image/visual-reverse-image-search-v2_intro.jpg"
+        imageURL:
+          "https://helpx.adobe.com/content/dam/help/en/stock/how-to/visual-reverse-image-search/jcr_content/main-pars/image/visual-reverse-image-search-v2_intro.jpg",
       }
     }
 
     if (Component.getInitialProps) {
-      pageProps = await Component.getInitialProps(ctx);
+      pageProps = await Component.getInitialProps(ctx)
     }
-    let url = absolute;
-    let headersHost = ctx.req.headers.host;
-    return { pageProps, tenantObject, url, headersHost };
+    let url = absolute
+    let headersHost = ctx.req.headers.host
+    return { pageProps, tenantObject, url, headersHost }
   }
 
   render() {
-    const { Component, pageProps, tenantObject, url } = this.props;
+    const { Component, pageProps, tenantObject, url } = this.props
 
     return (
-      <TenantContext.Provider value={tenantObject}>
+      <TenantContext.Provider
+        value={{ value: tenantObject, db: db(tenantObject.hostUrl) }}
+      >
         <Page tenantObject={tenantObject}>
           <Component {...pageProps} />
         </Page>
       </TenantContext.Provider>
-    );
+    )
   }
 }

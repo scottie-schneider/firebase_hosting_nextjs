8b68a8d2242f29e7d2e7be8e81564e2aee1dfc76
Scottie Schneider <scott@laclave.co>
2020-02-24 22:47:45 -0500

    fetching data from db in get initial props and rendering to conversatoin list

--- src/server/index.js
+++ src/server/index.js
@@ -5,67 +5,83 @@ admin.initializeApp()
 import next from "next"
 
 // callable functions
+// const c = require("./callable")
 exports.testEmail = https.onCall(async (data, context) => {
-  console.log("heyyyyy email")
-})
-const c = require("./callable")
-exports.sendTestEmail = https.onCall(c.emails.sendEmail)
-exports.hi = https.onCall(c.emails.hi)
+	// connect to the db and pull in all messages with a workspace ID
+	let leads = []
+	const leadsRef = admin
+		.firestore()
+		.collection("leads")
+		.where("workspaceId", "==", "1")
 
-// scheduled functions
-const s = require("./scheduled")
-exports.runCampaignEvents = pubsub
-  .schedule("every 1 mins")
-  .onRun(s.campaignEvents.run)
+	const leadsData = await leadsRef.get()
+	leadsData.forEach((lead) => {
+		leads.push({ leadId: lead.id, ...lead.data() })
+	})
+	console.log(leads)
+	// pull all the workspace's leads
+
+	// connect the messages to the workspace leads
+
+	return leads
+})
+// exports.sendTestEmail = https.onCall(c.emails.sendEmail)
+// exports.hi = https.onCall(c.emails.hi)
+
+// // scheduled functions
+// const s = require("./scheduled")
+// exports.runCampaignEvents = pubsub
+// 	.schedule("every 1 mins")
+// 	.onRun(s.campaignEvents.run)
 
 const cors = require("cors")({ origin: true })
 // this is the function that renders our NextJS pages
 
 const dev = process.env.NODE_ENV !== "production"
 const app = next({
-  dev,
-  // the absolute directory from the package.json file that initialises this module
-  // IE: the absolute path from the root of the Cloud Function
-  conf: { distDir: "dist/client" },
+	dev,
+	// the absolute directory from the package.json file that initialises this module
+	// IE: the absolute path from the root of the Cloud Function
+	conf: { distDir: "dist/client" },
 })
 const handle = app.getRequestHandler()
 
 const server = https.onRequest((request, response) => {
-  // log the page.js file or resource being requested
-  console.log("File: " + request.originalUrl)
-  return app.prepare().then(() => handle(request, response))
+	// log the page.js file or resource being requested
+	console.log("File: " + request.originalUrl)
+	return app.prepare().then(() => handle(request, response))
 })
 
 exports.nextjs = {
-  server,
+	server,
 }
 
-exports.receiveEmail = https.onRequest((req, res) => {
-  cors(req, res, () => {
-    // TODO is this a new message or an old one?yarn ad
+// exports.receiveEmail = https.onRequest((req, res) => {
+// 	cors(req, res, () => {
+// 		// TODO is this a new message or an old one?yarn ad
 
-    // TODO is there another way besides the email we can id who is sending it?
+// 		// TODO is there another way besides the email we can id who is sending it?
 
-    // the text reply
-    console.log("********* stripped text reply ***********")
-    // only if the email is a reply!
-    console.log(req.body.StrippedTextReply)
+// 		// the text reply
+// 		console.log("********* stripped text reply ***********")
+// 		// only if the email is a reply!
+// 		console.log(req.body.StrippedTextReply)
 
-    console.log("******** TEXT BODY **********")
-    console.log(req.body.TextBody)
-    console.log("subject")
-    console.log(req.body.Subject)
-    // who it's from
-    console.log("from")
-    console.log(req.body.From)
-    // who it's to (the client)
-    console.log("to")
-    console.log(req.body.To)
+// 		console.log("******** TEXT BODY **********")
+// 		console.log(req.body.TextBody)
+// 		console.log("subject")
+// 		console.log(req.body.Subject)
+// 		// who it's from
+// 		console.log("from")
+// 		console.log(req.body.From)
+// 		// who it's to (the client)
+// 		console.log("to")
+// 		console.log(req.body.To)
 
-    console.log("************** html  **************")
-    console.log(req.body.HtmlBody)
-    // find the client, add a message on that lead's history. Include HTML and text
+// 		console.log("************** html  **************")
+// 		console.log(req.body.HtmlBody)
+// 		// find the client, add a message on that lead's history. Include HTML and text
 
-    return res.status(200).send({ message: "success" })
-  })
-})
+// 		return res.status(200).send({ message: "success" })
+// 	})
+// })
